e5406db3ba8645b6ddd5293eb5d82c3621974935
NoorBayat <103617655+NoorBayat@users.noreply.github.com>
2022-08-11 10:39:22 +0800

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,98 @@
+const products = [
+  {
+    id: 'rec43w3ipXvP28vog',
+    title: 'HTML 5',
+    company: 'Website',
+    image:
+      'img/Book13.jpg',
+    author: 'John B',
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: '3D Objects',
+    company: 'Modern Art',
+    image:
+      'img/bookCover.jpg',
+    author: 'Mark H',
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'Filter Effects',
+    company: 'Photography',
+    image:
+      'img/Book4.jpg',
+    author:'Simons Hiei',
+  },
+  {
+    id: 'recBohCqQsot4Q4II',
+    title: 'Digital Photography',
+    company: 'Photography',
+    image:
+      'img/ModernArts.jpg',
+    author: 'Hyden Sinhe',
+  },
+  {
+    id: 'recDG1JRZnbpRHpoy',
+    title: 'Photography Now',
+    company: 'Photography',
+    image:
+      'img/photography.jpg',
+    author: 'Jacob MN',
+  },
+  {
+    id: 'recNWGyP7kjFhSqw3',
+    title: 'Bootstrap',
+    company: 'Website',
+    image:
+      'img/bootstrap.png',
+    author: 'Mark Xhue',
+  },
+  {
+    id: 'recZEougL5bbY4AEx',
+    title: 'Abstract',
+    company: 'Modern Art',
+    image:
+      'img/Book3.jpg',
+    author: 'Lei Dong',
+  },
+  {
+    id: 'recjMK1jgTb2ld7sv',
+    title: 'Php ',
+    company: 'Programming',
+    image:
+      'img/php.jpeg',
+    author: 'Akash Chopra',
+  },
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    title: 'Marcel Duchamp',
+    company: 'Modern Art',
+    image:
+      'img/Book6.jpg',
+    author: 'Joseph khdu',
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    title: 'ASP.NET',
+    company: 'Programming',
+    image:
+      'img/asp.jpg',
+    author: 'AKhdhe ahdh',
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    title: 'CSS Tricks',
+    company: 'Website',
+    image:
+      'img/Book2.jpg',
+    author: 'Jhony Deplin',
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'C# in Depth ',
+    company: 'Programming',
+    image:
+      'img/Cindept.jpg',
+    author: 'Hudie Dhie',
+  },
+];
